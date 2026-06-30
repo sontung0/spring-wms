070794b10c510f632ade43a8a3c34b82089fbb91
sontung0 <sontung0@gmail.com>
2026-06-30 15:50:17 +0700

    feat: add UserCreatedEvent and UserUpdatedEvent domain events

---
+++ app/src/main/java/nst/wms/user/domain/events/UserUpdatedEvent.java
@@ -0,0 +1,17 @@
+package nst.wms.user.domain.events;
+
+import nst.wms.common.event.ExternalEvent;
+import nst.wms.user.domain.User;
+
+public record UserUpdatedEvent(User user) implements ExternalEvent {
+
+    @Override
+    public String getEventTarget() {
+        return "users";
+    }
+
+    @Override
+    public String getEventKey() {
+        return user.getId().toString();
+    }
+}
