b15adaa93df4c04ee35f8d62778f24d866250859
sontung0 <sontung0@gmail.com>
2026-07-11 10:43:54 +0700

    feat: add UserEventConsumer with sendUserCreatedNoti and sendUserUpdatedNoti

---
+++ worker/src/main/java/nst/wms/worker/config/UserEventConsumer.java
@@ -0,0 +1,26 @@
+package nst.wms.worker.config;
+
+import nst.wms.user.domain.events.UserCreatedEvent;
+import nst.wms.user.domain.events.UserUpdatedEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.function.Consumer;
+
+@Configuration
+class UserEventConsumer {
+
+    private static final Logger log = LoggerFactory.getLogger(UserEventConsumer.class);
+
+    @Bean
+    Consumer<UserCreatedEvent> sendUserCreatedNoti() {
+        return event -> log.info("Sending notification for created user: {}", event.user().getEmail());
+    }
+
+    @Bean
+    Consumer<UserUpdatedEvent> sendUserUpdatedNoti() {
+        return event -> log.info("Sending notification for updated user: {}", event.user().getEmail());
+    }
+}
